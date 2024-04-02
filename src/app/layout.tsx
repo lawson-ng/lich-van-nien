86a062fcb8790230332a2f356fda6b65d45f14ad
lawson-ng <abraham.lawson.dev@gmail.com>
2024-04-02 13:38:59 +0700

    add gg ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
         />
       </head>
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-0C46PW5Y0C" />
     </html>
   );
 }
